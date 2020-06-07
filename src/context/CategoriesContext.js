2c8b5f146b7f83d6722a079fffd6c4c52c4b4314
AlanAlv <alan.alv96@gmail.com>
2020-06-07 04:02:35 -0500

    API Call

---
+++ src/context/CategoriesContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+// Create context
+export const CategoriesContext = createContext();
+
+// Provider with state and functions
+const CategoriesProvider = (props) => {
+
+    // Context state 
+    const [ categories, saveCategories ] = useState([]);
+
+    // API Call
+    useEffect(() => {
+        const getCategories = async () => {
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`;
+
+            const categories = await axios.get(url);
+            saveCategories(categories.data.drinks);
+        }
+        getCategories();
+    }, [])
+
+    return (
+        <CategoriesContext.Provider
+            value={{
+                categories
+            }}
+        >
+            {props.children}
+        </CategoriesContext.Provider>
+    )
+}
+
+export default CategoriesProvider;

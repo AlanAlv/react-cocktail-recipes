e34901e4a454dc985b6c700fa71e17246e71541b
AlanAlv <alan.alv96@gmail.com>
2020-06-08 02:51:25 -0500

    Recipe Details API Call

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -9,6 +9,21 @@ const ModalProvider = (props) => {
     // Provider State
     const [ recipeId, saveRecipeId ] = useState(null);
 
+    // CallAPI
+    useEffect(() => {
+        const getRecipe = async () => {
+            if(!recipeId) return;
+                
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${recipeId}
+            `;
+
+            const result = await axios.get(url);
+            console.log(result.data.drinks[0]);
+        }
+        getRecipe()
+
+    }, [recipeId])
+
     return(
         <ModalContext.Provider
             value={{saveRecipeId}}
